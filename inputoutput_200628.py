3687fc659bcbc7a75a678158080817704aa86d4c
Heegene <dev.heejin@gmail.com>
2020-06-28 18:36:38 +0900

    200628 POSTECH python week1 ex

---
+++ inputoutput_200628.py
@@ -0,0 +1,25 @@
+# name = input("이름이 무엇인가요?")
+# print("만나서 반갑습니다." + name + "님!")
+
+# a = 500
+# b = 100
+# print(a+b)
+
+x = input("첫 번째 정수: ")
+y = input ("두 번째 정수: ")
+sum = x + y
+print("합은 " , sum)
+
+# 이렇게 하면 출력결과가 1020 이렇게 String의 합으로 나타남
+# 숫자로 형변환을 해 줌
+
+
+name = input("이름을 입력해 주세요.")
+print(name + " 님 반갑습니다.")
+age = input('나이를 입력해 주세요')
+print('10년 후면 ' , int(age)+10 , '살이 되시는군요 !')
+
+# String 더하기가 아니라 중간에 int 타입이 들어가므로
+# + 를 사용하지 않고 , , 를 사용하여 concatenate처리
+
+
